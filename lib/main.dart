2151ca1b4bfc5325ea2c187c382a3a9a5e0cf7d6
Saurabh Grewal <saurabhgrewal718@gmail.com>
2020-06-15 22:34:27 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Scaffold(
+      appBar: AppBar(title:Text('Quiz App')),
+      body: Text('This is my Quiz app'),
+     ),
+    );
+  }
+}
