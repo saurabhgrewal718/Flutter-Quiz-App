a8d797489d49da18e42c59ec528051806307932f
Saurabh Grewal <saurabhgrewal718@gmail.com>
2020-06-15 23:14:26 +0530

    the buttons added and the screen shows simple text with three buttons which uses the anonmous functions

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,29 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerquestion(){
+    print("i am the answer");
+  }
   
   @override
   Widget build(BuildContext context) {
+    var questions=[
+      "what's is your favourite color",
+      "what's is your favourite game",
+      "what's is your favourite class",
+      "what's is your favourite deodrent",
+    ];
     return MaterialApp(
       home:Scaffold(
       appBar: AppBar(title:Text('Quiz App')),
-      body: Text('This is my Quiz app'),
+      body: Column(children: <Widget>[
+        Text("this is my text"),
+        RaisedButton(child: Text("Answer 1"),onPressed: answerquestion),
+        RaisedButton(child: Text("Answer 2"),onPressed: ()=> print("i am the question 2")),
+        RaisedButton(child: Text("Answer 3"),onPressed: ()=> print("i am the question 3")),
+      ],
      ),
+    ),
     );
   }
 }
